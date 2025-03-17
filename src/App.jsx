66bf852ad4de7f34b9e322965d91ab431e41b1c4
abderrahmane <abderrahmanmlih9@gmail.com>
2025-03-17 02:03:25 +0000

    install react and setting tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex 
+    flex-col items-center justify-center bg-blue-100">
+      <h1>Weather App</h1>
+
+
+    </div>
+
+  )
+}
+
+export default App
